6e5e19539ed898cf2226acfdde8ba9eaac62a3b6
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2022-04-07 08:56:10 +0800

    remove onCreateNode

--- packages/source/gatsby-node.js
+++ packages/source/gatsby-node.js
@@ -1,6 +1,5 @@
 const { createSchemaCustomization } = require('./build/createSchemaCustomization')
-const { onCreateNode } = require('./build/onCreateNode')
 const { pluginOptionsSchema } = require('./build/pluginOptionsSchema')
 const { sourceNodes } = require('./build/sourceNodes')
 
-module.exports = { createSchemaCustomization, onCreateNode, pluginOptionsSchema, sourceNodes }
+module.exports = { createSchemaCustomization, pluginOptionsSchema, sourceNodes }
